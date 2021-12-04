2edb14f79bc2acb1803646fe339105f3cbc36bcc
Elliot <elliotcoy@gmail.com>
2021-12-03 23:51:09 -0400

    D1P2

---
+++ Day 1/P2/solution.py
@@ -0,0 +1,12 @@
+s =  open('Day 1\P2\input.txt', 'r').read()
+list = [int(x) for x in s.splitlines()]
+out = 0
+
+for i in range(len(list)-2):
+    if i > 0:
+        a = list[i-1] + list[i] + list[i+1]
+        b = list[i] + list[i+1] + list[i+2]
+        if b > a:
+            out += 1
+
+print(out)
---
+++ Day 2/P1/solution.py
@@ -0,0 +1,11 @@
+import sys
+
+out = 0
+x = 0
+y = 0
+
+with open('Day 2\P1\input.txt', 'r') as s:
+    for line in s:
+        out = 0 #todo
+
+print(out)
