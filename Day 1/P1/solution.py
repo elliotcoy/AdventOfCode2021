fd95f54feb1433509e20930ae3d5552d6bcda068
Elliot <elliotcoy@gmail.com>
2021-12-03 23:24:16 -0400

    Day 1 Puzzle 1 Complete
    Fix

---
+++ Day 1/P1/solution.py
@@ -0,0 +1,14 @@
+import sys
+
+prv = sys.maxsize
+cur = 0
+out = 0
+
+with open('Day 1\P1\input.txt', 'r') as s:
+    for line in s:
+        cur = int(line)
+        if cur > prv:
+            out += 1
+        prv = cur
+
+print(out)
