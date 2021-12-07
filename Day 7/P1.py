ea1237e2cbd71e7dff0bdeadb4aac6cb081a581a
Elliot <elliotcoy@gmail.com>
2021-12-07 03:10:11 -0400

    D7
    output format

---
+++ Day 7/P1.py
@@ -0,0 +1,23 @@
+
+crabs = list(map(int,open('Day 7\input.txt','r').read().split(',')))
+
+def abs(n):
+    if n < 0:
+        return n*-1
+    return n
+
+bestCost = -1
+bestx = -1
+
+for xpos in range(min(crabs), max(crabs)):
+
+    cost = 0
+    for x in crabs:
+        cost += abs(x-xpos)
+    
+    if cost < bestCost or bestCost < 0:
+        bestCost = cost
+        bestx = xpos
+        
+    
+print(f'x: {bestx}  |  {bestCost} fuel.')
---
+++ Day 7/P2.py
@@ -0,0 +1,27 @@
+
+crabs = list(map(int,open('Day 7\input.txt','r').read().split(',')))
+
+def abs(n):
+    if n < 0:
+        return n*-1
+    return n
+
+def sumcount(n):
+    return int(n*(n+1)/2)
+
+bestCost = -1
+bestx = -1
+
+for xpos in range(min(crabs), max(crabs)):
+
+    cost = 0
+    for x in crabs:
+        steps = abs(x-xpos)
+        cost += sumcount(steps)
+    
+    if cost < bestCost or bestCost < 0:
+        bestCost = cost
+        bestx = xpos
+        
+    
+print(f'x: {bestx}') ; print(f'fuel: {bestCost}')
